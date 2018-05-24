ea448cd81fd0fb4f5a47fed2332782801bc11111
Max Zheng <zz5ra@virginia.edu>
2018-05-24 15:26:14 -0500

    Finished all graphics, need to refactor

--- gamelogic.js
+++ gamelogic.js
@@ -3,12 +3,13 @@ var gameText = document.querySelector('.gameText');
 var displayWord = document.querySelector('.displayWord');
 var lettersGuessed = document.querySelector('.lettersGuessed');
 var displayMessage = document.querySelector('.displayMessage');
+var errorMessage = document.querySelector('.errorMessage');
 
 var guessPics = document.querySelector('.pics');
 
 var wordSubmitButton = document.querySelector('.wordSubmitButton');
+var randomWordButton = document.querySelector('.randomWordButton');
 var guessButton = document.querySelector('.guessButton');
-var secretWord = document.querySelector('.secretWord');
 var resetButton = document.querySelector('.resetButton');
 
 var guessCount = 5;
@@ -22,17 +23,46 @@ var gameStart = document.querySelector('.gameStart');
 
 var wordToDisplay = "";
 
+var secretWord;
+
+var wordDictionary;
+
 gameStart.style.display = 'none';
+errorMessage.style.display = 'none';
+
+document.querySelector('.secretWord').focus();
+
+var req = new XMLHttpRequest();
+req.onload = function(){
+  wordDictionary = this.responseText.split('\n');
+};
+req.open('GET', 'words.txt');
+req.send();
 
 function setCharAt(str,index,chr) {
     if(index > str.length-1) return str;
     return str.substr(0,index) + chr + str.substr(index+1);
 }
 
+function setUp2PlayerGame() {
+  secretWord = document.querySelector('.secretWord').value;
+  if(!isNonEmptyStr(secretWord)) {
+    errorMessage.style.display = 'initial';
+    return;
+  }
+  setupGame();
+}
+
+function setUpRandomGame() {
+  var index = Math.floor(Math.random() * wordDictionary.length);
+  secretWord = wordDictionary[index];
+  setupGame();
+}
+
 function setupGame() {
   intro.style.display = 'none';
 
-  for (i = 0; i < secretWord.value.length; i++) {
+  for (i = 0; i < secretWord.length; i++) {
       wordToDisplay += "_ ";
   }
 
@@ -54,9 +84,22 @@ function setupGame() {
 
   resetButton.style.display = 'none';
 
+  document.querySelector('.letterGuess').focus();
+
+}
+
+function isNonEmptyStr(str) {
+  return str.length >= 1 && str.match(/[a-z]/i);
 }
 
 function guessLetter() {
+
+  if(!isNonEmptyStr(letterGuess.value)) {
+    displayMessage.textContent = "Please guess a letter from a-z!"
+    displayMessage.style.display = 'initial';
+    return;
+  }
+
   if (guessCharSet.has(letterGuess.value)) {
     displayMessage.textContent = "You have already guessed that letter!";
     displayMessage.style.display = 'initial';
@@ -66,8 +109,8 @@ function guessLetter() {
   displayMessage.style.display = 'none';
 
   var letterFound = false;
-  for (var i = 0; i < secretWord.value.length; i++) {
-    if(letterGuess.value === secretWord.value[i]) {
+  for (var i = 0; i < secretWord.length; i++) {
+    if(letterGuess.value === secretWord[i]) {
       letterFound = true;
       lettersRevealed++;
       wordToDisplay = setCharAt(wordToDisplay,i*2,letterGuess.value);
@@ -85,25 +128,25 @@ function guessLetter() {
   lettersGuessed.textContent += letterGuess.value + ' ';
   guessesLeftMessage.textContent = "You have " + guessCount + " incorrect guesses remaining.";
 
-  checkWinOrLoss();
-
+  if(checkWinOrLoss() === false) {
+    letterGuess.focus();
+  }
 }
 
 function checkWinOrLoss() {
-  if(lettersRevealed === secretWord.value.length) {
-    displayMessage.style.display = 'initial';
-    displayMessage.textContent = 'You win!!!'
+  if(lettersRevealed === secretWord.length) {
+    alert("You win!!!");
     guessButton.disabled = true;
     resetButton.style.display = 'initial';
-    return;
+    return true;
   }
   if(guessCount === 0) {
-    displayMessage.style.display = 'initial';
-    displayMessage.textContent = 'You lose!!'
+    alert("You lose!!! The secret word was: " + secretWord);
     guessButton.disabled = true;
     resetButton.style.display = 'initial';
-    return;
+    return true;
   }
+  return false;
 }
 
 function resetGame() {
@@ -116,8 +159,12 @@ function resetGame() {
   wordToDisplay = "";
   gameStart.style.display = 'none';
   intro.style.display = 'initial';
+  errorMessage.style.display = 'none';
+
+  document.querySelector('.secretWord').focus();
 }
 
-wordSubmitButton.addEventListener('click', setupGame);
+wordSubmitButton.addEventListener('click', setUp2PlayerGame);
+randomWordButton.addEventListener('click', setUpRandomGame);
 guessButton.addEventListener('click', guessLetter);
 resetButton.addEventListener('click',resetGame);
